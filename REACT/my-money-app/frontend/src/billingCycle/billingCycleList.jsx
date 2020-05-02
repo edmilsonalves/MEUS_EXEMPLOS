ee1ec149ee76ad2d7c274d53c5e38ffcedcd7e33
Edmilson Alves dos Reis <edmilson@univision.net.br>
2020-05-02 17:09:15 -0300

    alterando as versoes

--- REACT/my-money-app/frontend/src/billingCycle/billingCycleList.jsx
+++ REACT/my-money-app/frontend/src/billingCycle/billingCycleList.jsx
@@ -1,54 +1,58 @@
-import React, { Component } from 'react'
-import { bindActionCreators } from 'redux'
-import { connect } from 'react-redux'
-import { getList, showUpdate, showDelete } from './billingCycleActions'
+import React, { useEffect } from "react";
+import { bindActionCreators } from "redux";
+import { connect } from "react-redux";
+import { getList, showUpdate, showDelete } from "./billingCycleActions";
 
-class BillingCycleList extends Component {
+const BillingCycleList = (props: any) => {
+  useEffect(() => {
+    props.getList();
+  }, []);
 
-    componentWillMount() {
-        this.props.getList()
-    }
+  function renderRows() {
+    const list = props.list || [];
+    return list.map((bc) => (
+      <tr key={bc._id}>
+        <td>{bc.name}</td>
+        <td>{bc.month}</td>
+        <td>{bc.year}</td>
+        <td>
+          <button
+            className="btn btn-warning"
+            onClick={() => props.showUpdate(bc)}
+          >
+            <i className="fa fa-pencil"></i>
+          </button>
+          <button
+            className="btn btn-danger"
+            onClick={() => props.showDelete(bc)}
+          >
+            <i className="fa fa-trash-o"></i>
+          </button>
+        </td>
+      </tr>
+    ));
+  }
 
-    renderRows() {
-        const list = this.props.list || []
-        return list.map(bc => (
-            <tr key={bc._id}>
-                <td>{bc.name}</td>
-                <td>{bc.month}</td>
-                <td>{bc.year}</td>
-                <td>
-                    <button className='btn btn-warning' onClick={() => this.props.showUpdate(bc)}>
-                        <i className='fa fa-pencil'></i>
-                    </button>
-                    <button className='btn btn-danger' onClick={() => this.props.showDelete(bc)}>
-                        <i className='fa fa-trash-o'></i>
-                    </button>
-                </td>
-            </tr>
-        ))
-    }
+  return (
+    <div>
+      <table className="table">
+        <thead>
+          <tr>
+            <th>Nome</th>
+            <th>Mês</th>
+            <th>Ano</th>
+            <th className="table-actions">Ações</th>
+          </tr>
+        </thead>
+        <tbody>{renderRows()}</tbody>
+      </table>
+    </div>
+  );
+};
 
-    render() {
-        return (
-            <div>
-                <table className='table'>
-                    <thead>
-                        <tr>
-                            <th>Nome</th>
-                            <th>Mês</th>
-                            <th>Ano</th>
-                            <th className='table-actions'>Ações</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        {this.renderRows()}
-                    </tbody>
-                </table>
-            </div>
-        )
-    }
-}
+export default BillingCycleList;
 
-const mapStateToProps = state => ({list: state.billingCycle.list})
-const mapDispatchToProps = dispatch => bindActionCreators({getList, showUpdate, showDelete}, dispatch)
-export default connect(mapStateToProps, mapDispatchToProps)(BillingCycleList)
+// const mapStateToProps = (state) => ({ list: state.billingCycle.list });
+// const mapDispatchToProps = (dispatch) =>
+//   bindActionCreators({ getList, showUpdate, showDelete }, dispatch);
+// export default connect(mapStateToProps, mapDispatchToProps)(BillingCycleList);
